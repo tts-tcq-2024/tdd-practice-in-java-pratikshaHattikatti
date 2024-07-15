96426aa3e93b090a058f6f7135bc261a2f861f7c
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-07-16 01:21:45 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -9,19 +9,29 @@ public class StringCalculator{
       if(inputNumbers == null || inputNumbers.isEmpty){
         return 0;
       }
+    if(!isAlphanumeriac){ 
 
+      
     inputNumbers = inputNumbers.replace ("\n",",");
-
+    inputNumbers = inputNumbers.replace("[^\\d,]",",");  
     String[] numbers = inputNumbers.split(",");
 
     for(String inputN : numbers){
+      if(inputN < 1000){
       if(!inputN.isEmpty()){
         total += Integer.parseInt(inputN.trim());
       }
+      } 
+    }
     }
     return total;
 
     
   }
+
+  private static boolean isAlphanumeriac( String inputStr){
+    return inputStr.matches("[a-zA-Z0-9]*");
+  }
+  
 
 }
