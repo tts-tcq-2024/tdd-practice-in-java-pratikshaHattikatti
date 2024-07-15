47dd61b5375403981bea88d723b8c5bf5ba295a7
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-07-16 01:00:55 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -3,5 +3,14 @@ package TddPracticeInJava;
 public class StringCalculator{
 
   int total = 0
+  String inputSeries;
+
+  public static int add( String inputNumbers){
+      if(inputNumbers == null || inputNumbers.isEmpty){
+        return 0;
+      }
+
+    
+  }
 
 }
