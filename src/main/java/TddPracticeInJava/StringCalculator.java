5f5a682d3549f8109b93c6d7216f74452af69529
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-07-16 15:36:22 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -3,15 +3,28 @@ package TddPracticeInJava;
 public class StringCalculator{
 
   public static int add( String inputNumbers){
-     int total = 0
-      if(inputNumbers == null || inputNumbers.isEmpty){
-        return 0;
+   
+   if (isEmptyString(str) || str == "0") {
+            return 0;
+        }
+    int total =  verifytoAdd(inputNumbers);
+    
+   return total;
+  }
+
+}
+ protected static boolean isEmptyString(String inputNumbers) {  
+if(inputNumbers == null || inputNumbers.isEmpty){
+        return true;
       }
-      
-    inputNumbers = inputNumbers.replace ("\n",",");
+return false;
+}
+
+protected static  verifytoAdd(String inputNumbers){
+   inputNumbers = inputNumbers.replace ("\n",",");
     inputNumbers = inputNumbers.replace("[^\\d,]",",");  
     String[] numbers = inputNumbers.split(",");
-
+    int total = 0
     for(String inputN : numbers){
       if(inputN < 1000){
         total += Integer.parseInt(inputN.trim());
@@ -21,4 +34,5 @@ public class StringCalculator{
     return total;
   }
 
-}
+
+
